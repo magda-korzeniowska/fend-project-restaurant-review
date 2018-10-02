745b6bfb8acf3e88b2a0843e538dc9cf8209ac71
magda-korzeniowska <m.korzeniowska@tlen.pl>
2018-10-02 12:55:04 +0200

    Add fetch event listener - showing content from cache

--- sw.js
+++ sw.js
@@ -30,3 +30,16 @@ self.addEventListener('install', function(event) {
     })
   );
 });
+
+/**
+ * Show content from cache
+ */
+
+ self.addEventListener('fetch', function(event) {
+   event.respondWith(
+     caches.match(event.request).then(function(response) {
+       if (response) return response;
+       return fetch(event.request);
+     })
+   );
+ });
